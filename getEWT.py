494e8ab79629cac94b3692b3dd4c241001d5df99
0xFF9197ull <m181204@163.com>
2025-07-26 22:01:27 +0800

    未完成

---
+++ getEWT.py
@@ -0,0 +1,20 @@
+from openpyxl import load_workbook
+import time
+from openpyxl.utils.exceptions import InvalidFileException
+
+'''
+用于多日统计的工具
+'''
+
+# 定义常量
+COMPLETE = 100
+PASS = 60
+
+#定义变量
+OUTPUT_DIR = ""
+INPUT_DIR = ""
+unpassName = []
+incompleteName = []
+#设定班级人数，确定表格范围
+NOS = int(input("班级人数,按表格中人名总数填写"))+3#NumberOfStudents,班级人数
+NOS = str(NOS)
